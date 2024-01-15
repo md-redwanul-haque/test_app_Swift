c18729ed7e0b74d8a463635ce48314df009648d1
md-redwanul-haque <redwan229900@gmail.com>
2024-01-15 17:09:41 +0600

    tab bar added

---
+++ test_app/ViewController/SignUpController.swift
@@ -0,0 +1,22 @@
+//
+//  SignUpViewController.swift
+//  test_app
+//
+//  Created by Redwan on 15/1/24.
+//
+
+import UIKit
+
+class SignUpController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Create An Account"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
