4cb401544eb4530b799bf4d3b47e4a6916f67b37
md-redwanul-haque <redwan229900@gmail.com>
2024-01-17 12:40:03 +0600

    complex ui added in profile

--- test_app/ViewController/ProfileController.swift
+++ test_app/ViewController/ProfileController.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
+import ActionKit
 
 class ProfileController: UIViewController {
     
     @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var notifySwitch: UISwitch!
+    @IBOutlet weak var notifyLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +20,44 @@ class ProfileController: UIViewController {
         
         self.profileImage.applyCorner(cornerRadious: self.profileImage.frame.width/2.0, borWidth: 0.0)
         
+        
+        self.notifySwitch.addControlEvent(.valueChanged){
+            
+            print("Switch --> \(self.notifySwitch.isOn)")
+            if self.notifySwitch.isOn {
+                
+                self.notifyLabel.text = "Notify Me When Order Processed"
+                
+            }else{
+                
+                self.notifyLabel.text = "Don't Notify Me When Order Processed"
+            }
+            
+            
+            
+        }
+        
+        
     }
+    
+    
+
+    
+    
+//    @IBAction func notifySwitchChanged (){
+//        
+//        print("Switch --> \(self.notifySwitch.isOn)")
+//        if self.notifySwitch.isOn {
+//            
+//            self.notifyLabel.text = "Notify Me When Order Processed"
+//            
+//        }else{
+//            
+//            self.notifyLabel.text = "Don't Notify Me When Order Processed"
+//        }
+//        
+//        
+//    }
+    
     
 }
